b6c60b4a526466a9814edb8e734e128382510f2e
Elizaveta Podolskaya <elizavetapodolskaya21@gmail.com>
2023-11-06 14:21:14 +0300

    Added GlobalExceptionHandling.

---
+++ Web/Middlewares/GlobalExceptionHandlingMiddleware.cs
@@ -0,0 +1,52 @@
+﻿using ApplicationCore.Exceptions;
+using FluentValidation;
+
+namespace Web.Middlewares;
+
+public class GlobalExceptionHandlingMiddleware : IMiddleware
+{
+    private readonly ILogger<GlobalExceptionHandlingMiddleware> _logger;
+
+    public GlobalExceptionHandlingMiddleware(ILogger<GlobalExceptionHandlingMiddleware> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (EntityNotFoundException e)
+        {
+            context.Response.StatusCode = StatusCodes.Status404NotFound;
+            await context.Response.WriteAsync(e.Message);
+        }
+        catch (OperationFailureException e)
+        {
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+            await context.Response.WriteAsync(e.Message);
+        }
+        catch (ValidationException e)
+        {
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+            await context.Response.WriteAsJsonAsync(e.Errors);
+        }
+        catch (AuthorizationException e)
+        {
+            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            await context.Response.WriteAsJsonAsync(e.Message);
+        }
+        catch (OperationCanceledException e)
+        {
+            _logger.LogInformation($"Request to {context.Request.Path} was cancelled");
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, e.Message);
+            context.Response.StatusCode = StatusCodes.Status503ServiceUnavailable;
+        }
+    }
+}
