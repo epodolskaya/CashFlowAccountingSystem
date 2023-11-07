68a58ff42c01467650f6484a6d9a4094ab4be7c3
Elizaveta Podolskaya <elizavetapodolskaya21@gmail.com>
2023-11-07 13:05:32 +0300

    AccountController funclionality created.

--- Web/Controllers/AccountController.cs
+++ Web/Controllers/AccountController.cs
@@ -1,12 +1,45 @@
 ﻿using Infrastructure.Identity.Constants;
+using Infrastructure.Identity.Features.Register;
+using Infrastructure.Identity.Features.SignIn;
+using Infrastructure.Identity.Features.SignOut;
+using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Web.Controllers;
 
 [ApiController]
-[Route("[controller]")]
-[Authorize(Policy = PolicyName.FinancialAnalyst)]
+[Route("[controller]/[action]")]
+[AllowAnonymous]
 public class AccountController : ControllerBase
 {
+    private readonly IMediator _mediator;
+
+    public AccountController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Login([FromBody] SignInCommand command, CancellationToken cancellationToken)
+    {
+        await _mediator.Send(command, cancellationToken);
+
+        return Ok();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Register([FromBody] RegisterCommand command, CancellationToken cancellationToken)
+    {
+        await _mediator.Send(command, cancellationToken);
+
+        return Ok();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> SignOut(CancellationToken cancellationToken)
+    {
+        await _mediator.Send(new SignOutCommand(), cancellationToken);
+        return Ok();
+    }
 }
