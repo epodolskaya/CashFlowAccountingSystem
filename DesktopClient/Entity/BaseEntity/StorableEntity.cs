2529752b22058900fd8bf1ae43f31e0948832b52
Elizaveta Podolskaya <elizavetapodolskaya21@gmail.com>
2023-11-24 22:48:56 +0300

    Removed redundant IEquitable implementation in StorableEntity.

--- DesktopClient/Entity/BaseEntity/StorableEntity.cs
+++ DesktopClient/Entity/BaseEntity/StorableEntity.cs
@@ -1,46 +1,6 @@
 ﻿namespace DesktopClient.Entity.BaseEntity;
 
-public abstract class StorableEntity : IEquatable<StorableEntity>
+public abstract class StorableEntity
 {
     public long Id { get; set; }
-
-    public bool Equals(StorableEntity? other)
-    {
-        if (ReferenceEquals(null, other))
-        {
-            return false;
-        }
-
-        if (ReferenceEquals(this, other))
-        {
-            return true;
-        }
-
-        return Id == other.Id;
-    }
-
-    public override bool Equals(object? obj)
-    {
-        if (ReferenceEquals(null, obj))
-        {
-            return false;
-        }
-
-        if (ReferenceEquals(this, obj))
-        {
-            return true;
-        }
-
-        if (obj.GetType() != GetType())
-        {
-            return false;
-        }
-
-        return Equals((StorableEntity)obj);
-    }
-
-    public override int GetHashCode()
-    {
-        return Id.GetHashCode();
-    }
 }
